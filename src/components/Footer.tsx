dded997fd9c63b1bbabbaecd8a439500979efb10
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-18 19:07:31 +0100

    added header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <Container className="Footer">
+                </Container>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+import { toast, ToastContainer } from 'react-toastify';
+
+export default function Header() {
+    return (
+        <Navbar bg="light" sticky="top" className="Header">
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+            <Container fluid>
+                <Navbar.Brand>Trucksim dashboard</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
