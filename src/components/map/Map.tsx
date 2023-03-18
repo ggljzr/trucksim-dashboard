aebdc5943eae723aea0cbaae3a67c280f2d41d99
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-18 20:18:22 +0100

    basic map component

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+export default function Map() {
+    return (
+        <div className="App">
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+                integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+                crossOrigin="" />
+
+            <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+                integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+                crossOrigin=""></script>
+
+            {process.env.PUBLIC_URL}
+            <MapContainer center={[0, 0]} zoom={3} minZoom={3} scrollWheelZoom={false} style={{ height: "600px" }}>
+                <TileLayer
+                    attribution='https://github.com/Unicor-p/SCS_Map_Tiles'
+                    url={process.env.PUBLIC_URL + "/SCS_Map_Tiles/ats/latest/Tiles/{z}/{x}/{y}.png"}
+                />
+            </MapContainer>
+        </div>);
+}
--- src/pages/AdvisorPage.tsx
+++ src/pages/AdvisorPage.tsx
@@ -1,9 +1,11 @@
 import Body from '../components/Body';
 
+import Map from '../components/map/Map';
+
 export default function AdvisorPage() {
     return (
         <Body>
-            Advisor
+            <Map />
         </Body>
     );
 }
