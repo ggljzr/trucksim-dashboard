8a46d09f879fd4cd081489e3d04461aaea50010a
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-18 21:36:58 +0100

    bounded map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,6 +1,9 @@
+import { LatLngBounds } from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet'
 
 export default function Map() {
+    const bounds = new LatLngBounds([-94.52288485802637, 176.98426154872845], [94.78431788146456, -187.69750005228602])
+
     return (
         <div>
             <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
@@ -11,11 +14,11 @@ export default function Map() {
                 integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
                 crossOrigin=""></script>
 
-            <MapContainer center={[0, 0]} zoom={3} minZoom={3} scrollWheelZoom={false} className="Map">
+            <MapContainer center={[0, 0]} zoom={3} minZoom={3} maxZoom={8} className="Map" maxBounds={bounds}>
                 <TileLayer
                     attribution="<a href='https://github.com/Unicor-p/SCS_Map_Tiles'>Unicor-p</a>"
                     url={process.env.PUBLIC_URL + "/SCS_Map_Tiles/ats/latest/Tiles/{z}/{x}/{y}.png"}
                 />
             </MapContainer>
-        </div>);
+        </div >);
 }
