10f467d632aa52499e2513d477a06fcdbf0e7107
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-22 23:42:49 +0100

    job page

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,4 +1,6 @@
-import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import { GeoAltFill, BuildingFill, StopwatchFill, CurrencyExchange, BoxSeamFill, Globe } from "react-bootstrap-icons";
+
 
 import Body from '../components/Body';
 
@@ -8,32 +10,54 @@ interface Props {
     job?: Job,
 }
 
-
-
 export default function JobPage({ job }: Props) {
+    const iconSize = 24;
     // render job as a list if it exists
     const jobContent = (
-        <ListGroup>
-            <ListGroup.Item>{job?.destination_city}</ListGroup.Item>
-            <ListGroup.Item>{job?.destination_company}</ListGroup.Item>
-            <ListGroup.Item>{job?.delivery_time}</ListGroup.Item>
-            <ListGroup.Item>{job?.income}</ListGroup.Item>
-            <ListGroup.Item>{job?.cargo}</ListGroup.Item>
-            <ListGroup.Item>{job?.cargo} ({job?.cargo_mass} kg)</ListGroup.Item>
-            <ListGroup.Item>{job?.job_market}</ListGroup.Item>
-        </ListGroup>
+        <div>
+            <h2>Current job</h2>
+            <Table size='sm' bordered className='DashboardTable'>
+                <tbody>
+                    <tr>
+                        <td><GeoAltFill className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Destination</td>
+                        <td>{job?.destination_city}</td>
+                    </tr>
+                    <tr>
+                        <td><BuildingFill className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Company</td>
+                        <td>{job?.destination_company}</td>
+                    </tr>
+                    <tr>
+                        <td><StopwatchFill className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Delivery time</td>
+                        <td>{job?.delivery_time}</td>
+                    </tr>
+                    <tr>
+                        <td><CurrencyExchange className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Income</td>
+                        <td>{job?.income}</td>
+                    </tr>
+                    <tr>
+                        <td><BoxSeamFill className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Cargo</td>
+                        <td>{job?.cargo} ({job?.cargo_mass.toFixed(1)} kg)</td>
+                    </tr>
+                    <tr>
+                        <td><Globe className='DashboardTableIcon' size={iconSize} /></td>
+                        <td className='DashboardTableHeader'>Market</td>
+                        <td>{job?.job_market}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
     )
 
     return (
         <Body>
-            {
-                job === undefined ?
-                    <div className='d-flex justify-content-center'>
-                        <h1>No job selected</h1>
-                    </div>
-                    :
-                    jobContent
-            }
+            <div>
+                {job == undefined ? <h1>No job selected</h1> : jobContent}
+            </div>
         </Body>
     );
 }
