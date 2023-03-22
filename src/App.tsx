1cc1d237997240f49b3675600301a989250b6cff
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-22 23:21:10 +0100

    new testjob

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,25 @@ import { Job } from './types';
 import './App.css';
 
 const testJob: Job = {
-  cargo_id: "test",
-  cargo: "test",
-  cargo_mass: 123,
-  destination_city: "test",
-  destination_company: "test",
-  is_cargo_loaded: false,
-  job_market: "test",
-  delivery_time: "test",
-  income: 5555,
+  'cargo': 'Space Container',
+  'cargo_id': 'space_cont',
+  'cargo_loaded': true,
+  'cargo_mass': 12020.2001953125,
+  'cargo_unit_count': 1,
+  'cargo_unit_mass': 12020.2001953125,
+  'delivery_time': 100135,
+  'destination_city': 'Raton',
+  'destination_city_id': 'raton',
+  'destination_company': 'Rail Export',
+  'destination_company_id': 're_train',
+  'income': 66445,
+  'is_special_job': false,
+  'job_market': 'quick_job',
+  'planned_distance_km': 1375,
+  'source_city': 'Houston',
+  'source_city_id': 'houston',
+  'source_company': 'Space Center',
+  'source_company_id': 'spc_whs'
 }
 
 function App() {
