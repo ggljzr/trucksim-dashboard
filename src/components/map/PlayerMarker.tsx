20013a8b31ba8d77d8fa4ce13fcafabf2b1e5fcc
Ondra Červenka <gogo.lejzr@gmail.com>
2023-03-26 19:04:02 +0200

    player icon

--- src/components/map/PlayerMarker.tsx
+++ src/components/map/PlayerMarker.tsx
@@ -1,4 +1,5 @@
-import { useMap, Circle } from 'react-leaflet'
+import { Icon } from 'leaflet';
+import { useMap, Marker } from 'react-leaflet'
 
 import { DPlacement } from '../../types';
 import { dPlacementToPoint } from '../../utils';
@@ -12,10 +13,16 @@ interface Props {
  */
 export default function PlayerMarker({ currentPlacement }: Props) {
     const map = useMap();
-
     const position = (currentPlacement === null) ? null : map.unproject(dPlacementToPoint(currentPlacement), map.getMaxZoom());
 
+    const icon = new Icon({
+        iconUrl: 'logo192.png',
+
+        iconSize: [20, 20], // size of the icon
+        rotationAngle: 10,
+    })
+
     return position === null ? null : (
-        <Circle center={position} radius={2500} />
+        <Marker position={position} icon={icon} />
     )
 }
